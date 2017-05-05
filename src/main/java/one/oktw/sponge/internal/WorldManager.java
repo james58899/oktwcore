aeeff32ffb5b86f4cb0a842bb19507ec6028a74a
james58899 <james59988@gmail.com>
2017-05-06 00:07:41 +0800

    world manager update

--- src/main/java/one/oktw/sponge/internal/WorldManager.java
+++ src/main/java/one/oktw/sponge/internal/WorldManager.java
@@ -4,6 +4,11 @@ import one.oktw.sponge.Core;
 import org.slf4j.Logger;
 import org.spongepowered.api.Server;
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.world.WorldArchetype;
+import org.spongepowered.api.world.storage.WorldProperties;
+
+import java.io.IOException;
+import java.util.UUID;
 
 import static one.oktw.sponge.Core.getCore;
 
@@ -12,27 +17,29 @@ public class WorldManager {
     private Logger logger = core.getLogger();
     private Server server = Sponge.getServer();
 
-//    public void createWorld(String name) {
-//        logger.info("Create World: " + name);
-//        try {
-//            WorldProperties worldProperties = server.createWorldProperties(name, WorldArchetype.builder()
-//                    .keepsSpawnLoaded(false)
-//                    .enabled(true)
-//                    .generateSpawnOnLoad(false)
-//                    .loadsOnStartup(false)
-//                    .randomSeed()
-//                    .build(name, name)
-//            );
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public boolean checkExist(String name) {
-//        return server.getWorldProperties(name).isPresent();
-//    }
-//
-//    public Collection<WorldProperties> listWorld() {
-//        //TODO
-//    }
+    public void createWorld() {
+        String uuid = UUID.randomUUID().toString();
+        logger.info("Create World: " + uuid);
+        try {
+            WorldProperties worldProperties = server.createWorldProperties(uuid, WorldArchetype.builder()
+                    .keepsSpawnLoaded(false)
+                    .enabled(true)
+                    .generateSpawnOnLoad(false)
+                    .loadsOnStartup(false)
+                    .randomSeed()
+                    .build(uuid, uuid)
+            );
+        } catch (IOException e) {
+            logger.error("Create world failed!", e);
+        }
+    }
+
+    public void removeWorld(String uuid) {
+        logger.info("Delete World: " + uuid);
+        if (server.getWorldProperties(uuid).isPresent()) {
+            server.deleteWorld(server.getWorldProperties(uuid).get());
+        } else {
+            logger.error("World not found!");
+        }
+    }
 }
